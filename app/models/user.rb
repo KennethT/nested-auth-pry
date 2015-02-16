e7245b71d73aad0d3591779bb5aee569b808d2ce
Kenneth To <kennethkto@gmail.com>
2015-02-16 15:59:51 -0700

    add a user model with a secure password

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # this gives us additional method such as
+  # ".authenticate(password)", which salts and hashes the password; and compares it with the password_digest field
+  # - *.password = : takes a plain text password, salts and hashes it, and
+  # stores it in the database
+  has_secure_password
+end
