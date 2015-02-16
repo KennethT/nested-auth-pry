5094659366f53c0a3b374e401d7cf1b82d7d243f
Kenneth To <kennethkto@gmail.com>
2015-02-16 16:48:17 -0700

    add view users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+#sessionscontroller is a convention
+#bcrypt does not require on it being called sessionscontroller
+class SessionsController < ApplicationController
+
+  def create
+    
+  end
+
+  def destroy
+
+  end
+
+end
