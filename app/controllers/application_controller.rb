b33e7fe288b288ac11ed11fe21f1b41e10c9b9f6
Kenneth To <kennethkto@gmail.com>
2015-02-16 18:02:29 -0700

    allow user to know they are signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # current_user is only convention, bcrypt does not require this
+  def current_user
+    #this is the same as
+    # @current_user = @current_user || User.find_by(id :session[:user_id])
+    # This will return @current_user if it's already defined, otherwise
+    # will try to find the user stored in the session, assign it
+    # @current_user, and return the value
+    # if session[:user_id] is nil (meaning that th user is not logged in),
+    # .find_by will return nil, which will be assigned assigned to @current_user,
+    # and returned by the function
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # the code below is a long form description
+  # of @current_user ||= User.find_by(id: session[:user_id])
+  # long form description:
+  # if not @current_user
+  #   @current_user = User.find_by(id: session[:user_id])
+  # else
+  #   @current_user
+  # end
+
+  #allows us to call current_user
+  #in any view that inherits from
+  #ApplicationController
+  helper_method :current_user
 end
